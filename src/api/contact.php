1ba24a35639d667d2db66d7f74c60b62d6a6a998
meii181 <myangeldankouzo00@gmail.com>
2024-02-18 00:20:41 +0100

    fix

--- src/api/contact.php
+++ src/api/contact.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, PATCH, DELETE, PUT");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
--- src/api/signup.php
+++ src/api/signup.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, PATCH, DELETE, PUT");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
